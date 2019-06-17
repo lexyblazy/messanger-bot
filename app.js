f1abaab5be9ce9e09ac77ff9d72f536aa12ceaf1
Olalekan Adekola <lexyblazy@gmail.com>
2019-06-17 17:36:07 +0100

    added eslint to catch errors

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const app = express();
+const inquirer = require("inquirer");
 const PORT = process.env.PORT || 5000;
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout
+const { QUESTIONS } = require("./constants");
+const { evaluateAnswers } = require("./util");
+const askQuestions = () => {
+  return inquirer.prompt(QUESTIONS);
+};
+const Messages = require("./messages");
+
+const messages = new Messages();
+
+app.get("/messages", (req, res) => {
+  res.send(messages.getMessages());
 });
 
+app.get("/messages/:id", (req, res) => {
+  res.send(messages.getMessage(req.params.id));
+});
 
-app.listen(PORT, () => {
-  readline.question(`What's your name?\n`, name => {
-    console.log(`Hi ${name}!`);
-    readline.close();
-  });
+app.listen(PORT, async () => {
+  const answers = await askQuestions();
+  evaluateAnswers(answers);
+  messages.saveMessage(answers);
 });
